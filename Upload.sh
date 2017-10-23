7c884a286444d1d0b177f6ba660e170a743f3f00
Mike Zabek <mzabek@gmail.com>
2017-10-23 12:12:22 +0300

    Adding teaching and references list.

--- Upload.sh
+++ Upload.sh
@@ -3,5 +3,6 @@
 # Mike Zabek
 # 2017-07-27
 
-cd "/Users/zabek/Google Drive/website/UMichMirror"
-aws s3 sync ./ s3://mikezabek.com --exclude "*.DS_Store" --exclude ".git*" --exclude "Upload.sh" --delete
+cd "/Users/zabek/Google Drive/website/mikezabek"
+aws s3 sync s3://mikezabek.com/logs/ ./logs/ --delete
+aws s3 sync ./ s3://mikezabek.com --exclude "*.DS_Store" --exclude ".git*" --exclude "Upload.sh" --exclude "logs/" --delete
