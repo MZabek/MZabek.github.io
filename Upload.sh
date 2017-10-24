b33fbca1fc4e282c470988dd2c0ed55242417798
Mike Zabek <mzabek@gmail.com>
2017-10-24 18:55:16 +0300

    Not dealing with access logs.

--- Upload.sh
+++ Upload.sh
@@ -4,5 +4,4 @@
 # 2017-07-27
 
 cd "/Users/zabek/Google Drive/website/mikezabek"
-aws s3 sync s3://mikezabek.com/logs/ ./logs/ --delete
-aws s3 sync ./ s3://mikezabek.com --exclude "*.DS_Store" --exclude ".git*" --exclude "Upload.sh" --exclude "logs/" --delete
+aws s3 sync ./ s3://mikezabek.com --exclude "*.DS_Store" --exclude ".git*" --exclude "Upload.sh" --delete
